01126908930e93977119fc2be55ac2299edea4dd
Uyen Ho <uyen.ho@asnet.com.vn>
2022-12-16 14:52:06 +0700

    Create Footer component

---
+++ NEXTJS/nextjs-example/src/layouts/Footer/index.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react';
+
+const Footer = (): JSX.Element => {
+  return (
+    <Box as="footer" textAlign="center">
+      <Box as="p"> © 2022 - An Example Built with Next.js </Box>
+    </Box>
+  );
+};
+
+export default Footer;
