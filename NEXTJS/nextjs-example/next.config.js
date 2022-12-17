ef1d4fc6d0a8b98088a9b163212ef349de571233
Uyen Ho <uyen.ho@asnet.com.vn>
2022-12-17 21:52:39 +0700

    Config to images

--- NEXTJS/nextjs-example/next.config.js
+++ NEXTJS/nextjs-example/next.config.js
@@ -8,6 +8,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    domains: ['images.unsplash.com', 'wembleypark.com'],
+  },
 };
 
 module.exports = nextConfig;
