d3e01736786d7ca552ce3ffc039803650149ddc4
Uyen Ho <uyen.ho@asnet.com.vn>
2022-12-28 14:51:21 +0700

    Rename service

---
+++ NEXTJS/nextjs-13/services/index.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+import { BASE_URL } from 'constants/index';
+
+class EventService {
+  private axiosClient = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+      accept: 'application/json',
+    },
+  });
+
+  /**
+   * Get data from server
+   * @param url
+   * @returns
+   */
+  get<T>(url: string): Promise<T> {
+    return this.axiosClient.get(url);
+  }
+}
+
+const eventService = new EventService();
+
+export default eventService;
