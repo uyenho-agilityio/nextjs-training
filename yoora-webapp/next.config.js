d8fb455430f195ee8baf3115dbe85ee228a83c9d
Uyen Ho <uyen.ho@asnet.com.vn>
2023-01-14 10:39:32 +0700

    Update dependencies

--- yoora-webapp/next.config.js
+++ yoora-webapp/next.config.js
@@ -7,9 +7,7 @@ const nextConfig = {
 
   images: {
     domains: ['/public/images'],
-    allowFutureImage: true,
     deviceSizes: [320, 768, 1440],
-    // imageSizes: [272, 614, 1140],
   },
 };
 
