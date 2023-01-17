99e8b0053bb7f46a476c9fac125f583723904f12
Uyen Ho <uyen.ho@asnet.com.vn>
2023-01-17 09:43:48 +0700

    Rename

---
+++ yoora-webapp/components/common/Textarea/Textarea.test.tsx
@@ -0,0 +1,23 @@
+// Lib
+import { render, screen } from '@testing-library/react';
+
+// Component
+import { Textarea } from '.';
+
+// Constant
+import { MESSAGE } from '@webapp/constants';
+
+describe('Textarea renders', () => {
+  test('should render Textarea component with default props', () => {
+    const { container } = render(<Textarea value={MESSAGE.SEND_MESSAGE} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test('should simulate textarea event & display new message after typing', () => {
+    render(<Textarea placeholder="Message" />);
+    const textarea = screen.getByPlaceholderText(/message/i);
+
+    textarea.innerHTML = `${MESSAGE.SEND_MESSAGE}`;
+    expect(screen.getByText(/lorem/i)).toBeTruthy();
+  });
+});
