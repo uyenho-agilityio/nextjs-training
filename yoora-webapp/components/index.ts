c0022e8ad9d072a19c73acaf9a6b9d597e3bec1b
Uyen Ho <uyen.ho@asnet.com.vn>
2023-02-03 23:03:14 +0700

    Create authGuard component

--- yoora-webapp/components/index.ts
+++ yoora-webapp/components/index.ts
@@ -5,15 +5,16 @@ export { default as Footer } from './Footer';
 export { default as ContactForm } from './Form/Contact';
 export { default as LoginForm } from './Form/Login';
 export { default as MainNavigation } from './MainNavigation';
+export { default as AuthGuard } from './AuthGuard';
 
 export type { CardProps as FeaturesCardProps } from './Cards/FeaturesCard';
 export type { CardProps as WhatsNewCardProps } from './Cards/WhatsNewCard';
-export type { CardProps as FeedbackCardProps } from './Cards/FeedbackCard';
 export type { CardProps as MoreTopicsCardProps } from './Cards/MoreTopicsCard';
 
 export { BenefitCard } from './Cards/BenefitCard';
 export { FeaturesCard } from './Cards/FeaturesCard';
 export { WhatsNewCard } from './Cards/WhatsNewCard';
 export { FeedbackCard } from './Cards/FeedbackCard';
+export { FeedbackList } from './Cards/FeedbackCard/FeedbackList';
 export { MoreTopicsCard } from './Cards/MoreTopicsCard';
 export { BannerCard } from './Cards/BannerCard';
