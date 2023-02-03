e32b46608d68704c81fce2f1d74e60925d8bc9a6
Uyen Ho <uyen.ho@asnet.com.vn>
2023-02-03 22:48:10 +0700

    Update more types

--- yoora-webapp/types/index.ts
+++ yoora-webapp/types/index.ts
@@ -16,3 +16,33 @@ export type MenuList = {
   title: string;
   subMenu: SubMenuList[];
 };
+
+// SSR: Server-side rendering
+// SSG: Static-site generation
+// ISR: Incremental Static Regeneration
+export type Methods = 'SSR' | 'SSG' | 'ISR';
+
+export type FetchingOptions = {
+  endpoint: RequestInfo | URL;
+  method: Methods;
+};
+
+export enum ToastPosition {
+  Top = 'top',
+  TopRight = 'top-right',
+  TopLeft = 'top-left',
+  Bottom = 'bottom',
+  BottomRight = 'bottom-right',
+  BottomLeft = 'bottom-left',
+}
+
+export enum ToastStatus {
+  Success = 'success',
+  Error = 'error',
+}
+
+export enum ApiPath {
+  Messages = 'messages',
+  Login = 'login',
+  Logout = 'logout',
+}
