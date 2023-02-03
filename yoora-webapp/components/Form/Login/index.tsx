a0680553730bfa26c20e6f03e9f411fc68052d26
Uyen Ho <uyen.ho@asnet.com.vn>
2023-02-03 23:01:08 +0700

    Implement the login feature

--- yoora-webapp/components/Form/Login/index.tsx
+++ yoora-webapp/components/Form/Login/index.tsx
@@ -7,10 +7,16 @@ import Image from 'next/image';
 // Constants
 import { LOGO, TEXT } from '@webapp/constants';
 
+// Hook
+import { useAuth } from '@webapp/hooks';
+
 // Components
 import { Title, Input, InputGroup, Button } from '@webapp/components';
 
 const LoginForm = (): JSX.Element => {
+  const { state, handleChangeInput, handleValidateInput, handleSubmit } = useAuth();
+  const { email, password, error, loading } = state;
+
   return (
     <Container
       className="login-form"
@@ -27,27 +33,37 @@ const LoginForm = (): JSX.Element => {
       </Title>
 
       <Input
-        type="text"
-        text="Email *"
-        placeholder="Email"
         chakraInputProps={{ variant: 'login-form' }}
         formControlProps={{
           pos: 'relative',
           _focusWithin: { color: 'primary-btn-cl' },
         }}
+        type="text"
+        text="Email *"
+        onChange={e => handleChangeInput(e, 'email')}
+        onBlur={e => handleValidateInput(e, 'email')}
+        error={error.email}
       />
       <InputGroup
-        type="password"
-        text="Password *"
-        placeholder="Password"
         chakraInputProps={{ variant: 'login-form' }}
         chakraInputElementProps={{ mr: 2, pb: 3 }}
         formControlProps={{
           pos: 'relative',
           _focusWithin: { color: 'primary-btn-cl' },
         }}
+        type="password"
+        text="Password *"
+        onChange={e => handleChangeInput(e, 'password')}
+        onBlur={e => handleValidateInput(e, 'password')}
+        error={error.password}
       />
-      <Button type="submit" variant="submit">
+      <Button
+        variant="submit"
+        type="submit"
+        onClick={handleSubmit}
+        isLoading={loading}
+        isDisabled={!email || !password || !!error.email || !!error.password}
+      >
         {TEXT.LOGIN}
       </Button>
     </Container>
