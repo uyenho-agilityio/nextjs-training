9eddd05314730511d29357d8751cc29d87f18b1a
Uyen Ho <uyen.ho@asnet.com.vn>
2023-02-03 23:02:06 +0700

    Implement the logout feature

--- yoora-webapp/components/Header/index.tsx
+++ yoora-webapp/components/Header/index.tsx
@@ -2,22 +2,44 @@
 
 // Libs
 import { Container, Link as ChakraLink, Flex, Box, ButtonGroup } from '@chakra-ui/react';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import Link from 'next/link';
 import Image from 'next/image';
 import dynamic from 'next/dynamic';
+import { useCallback } from 'react';
 
 // Constants
-import { LOGO, PAGE_ROUTES, ROUTE_LIST, TEXT } from '@webapp/constants';
+import { LOGO, PAGE_ROUTES, ROUTE_LIST, STORAGE_KEYS, TEXT } from '@webapp/constants';
 
-// Components
+// Type
+import { User } from '@webapp/models';
+
+// Component
 import { Button } from '@webapp/components';
+
+// Hook
+import { useAuth } from '@webapp/hooks';
+
+// Util
+import { getStorage } from '@webapp/utils';
+
 const MainNavigation = dynamic(() => import('@webapp/components').then(mod => mod.MainNavigation), {
   ssr: false,
 });
 
 const Header = (): JSX.Element => {
   const pathname = usePathname();
+  const router = useRouter();
+  const { isAuthenticated, handleLogOut } = useAuth();
+
+  const currentUser =
+    typeof window !== 'undefined'
+      ? (getStorage(STORAGE_KEYS.CURRENT_USER) as unknown as User)
+      : null;
+
+  const handleLogIn = useCallback(() => {
+    router.push(PAGE_ROUTES.LOGIN);
+  }, [router]);
 
   return (
     <Container as="header" className="header">
@@ -46,7 +68,11 @@ const Header = (): JSX.Element => {
         </Box>
 
         <ButtonGroup display={{ base: 'none', lg: 'block' }}>
-          <Button>{TEXT.LOGIN}</Button>
+          {isAuthenticated || currentUser?.email ? (
+            <Button onClick={handleLogOut}>{TEXT.LOGOUT}</Button>
+          ) : (
+            <Button onClick={handleLogIn}>{TEXT.LOGIN}</Button>
+          )}
           <Button size="sm" variant="info">
             {TEXT.TRY_FOR_FREE}
           </Button>
