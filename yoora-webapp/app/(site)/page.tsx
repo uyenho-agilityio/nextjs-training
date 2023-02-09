8de1518c02d16e83974fc101a3ac10e5d04ac13e
Uyen Ho <uyen.ho@asnet.com.vn>
2023-02-09 16:43:08 +0700

    Remove unused code

--- yoora-webapp/app/(site)/page.tsx
+++ yoora-webapp/app/(site)/page.tsx
@@ -1,26 +1,10 @@
-// Libs
-import { notFound } from 'next/navigation';
+// Lib
 import dynamic from 'next/dynamic';
-
-// Type
-import { Message } from '@webapp/models';
-
-// Service
-import { fetchMessages } from '@webapp/services';
 
 // Component
 const HomePage = dynamic(() => import('./HomePage'));
 
 const Home = (): JSX.Element => {
-  // const Home = async (): Promise<JSX.Element> => {
-  // const dataRes = await fetchMessages('SSR');
-  // const data: Message[] = dataRes.data;
-
-  // if (!data) {
-  //   notFound();
-  // }
-
-  // return <HomePage data={data} />;
   return <HomePage />;
 };
 
