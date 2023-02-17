657a2b95b2c22e087854f364c6af74676071407d
Uyen Ho <uyen.ho@asnet.com.vn>
2023-02-17 17:30:52 +0700

    Fix testing with useRouter

--- yoora-webapp/components/MainNavigation/MainNavigation.test.tsx
+++ yoora-webapp/components/MainNavigation/MainNavigation.test.tsx
@@ -1,8 +1,15 @@
 // Libs
 import { render } from '@testing-library/react';
 
+// Util
+import { mockNextRouter } from '@webapp/utils';
+
 // Component
 import MainNavigation from '.';
+
+beforeEach(() => {
+  mockNextRouter({ pathname: '/contact', asPath: '/contact' });
+});
 
 describe('MainNavigation renders', () => {
   test('should render MainNavigation component with default props', () => {
