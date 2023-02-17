fb2b6062b76c17e475e0ccb3c2e65f722015c9aa
Uyen Ho <uyen.ho@asnet.com.vn>
2023-02-17 21:27:18 +0700

    Create mock data

---
+++ yoora-webapp/mocks/index.ts
@@ -0,0 +1,36 @@
+export const TEXT = {
+  NAME_KEY: 'name',
+  MESSAGE_KEY: 'message',
+  EMAIL_KEY: 'email',
+  PWD_KEY: 'password',
+  INVALID_NAME: 'User',
+  INVALID_EMAIL: 'user@com',
+  INVALID_PWD: 'user@123',
+  VALID_NAME: 'Guest',
+  VALID_EMAIL: 'user@api.com',
+  VALID_PWD: 'User@123',
+};
+
+export const MESSAGE = {
+  INVALID_MESSAGE_INPUT:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia doloribus facilis magnam ut cum unde, pariatur minima delectus quae nostrum suscipit animi repellat accusantium tenetur, velit error quod officiis minus!',
+  VALID_MESSAGE_INPUT:
+    'Officia doloribus facilis magnam ut cum unde, pariatur minima delectus quae nostrum suscipit animi repellat accusantium tenetur, velit error quod officiis minus!',
+  INVALID_EMAIL: 'Email is invalid',
+  INVALID_PWD: 'Password is invalid',
+  INVALID_NAME: 'Name should be a minimum of five characters in length',
+  INVALID_MESSAGE: 'Message should be between 40 and 180 characters in length',
+  REQUIRED_PWD: 'Password is required',
+};
+
+export const USER_DATA = {
+  id: '1',
+  email: 'user@api.com',
+  password: 'User@123',
+  avatar: {
+    src: '/images/feedback-section/default-avatar-1.svg',
+    alt: 'avatar',
+    width: 48,
+    height: 48,
+  },
+};
