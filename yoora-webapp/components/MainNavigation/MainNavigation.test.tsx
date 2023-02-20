bf1cd86d6faf8d27429b9db56cbee9fb2b7cd55b
Uyen Ho <uyen.ho@asnet.com.vn>
2023-02-20 15:54:35 +0700

    Fix testing with useRouter

--- yoora-webapp/components/MainNavigation/MainNavigation.test.tsx
+++ yoora-webapp/components/MainNavigation/MainNavigation.test.tsx
@@ -1,19 +1,40 @@
 // Libs
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { useRouter } from 'next/navigation';
 
-// Util
-import { mockNextRouter } from '@webapp/utils';
+// Constant
+import { API_PATH } from '@webapp/constants';
 
 // Component
 import MainNavigation from '.';
 
-beforeEach(() => {
-  mockNextRouter({ pathname: '/contact', asPath: '/contact' });
-});
+jest.mock('next/navigation', () => ({
+  useRouter: jest.fn(),
+  usePathname: jest.fn(),
+}));
 
 describe('MainNavigation renders', () => {
   test('should render MainNavigation component with default props', () => {
     const { container } = render(<MainNavigation size="full" />);
     expect(container).toMatchSnapshot();
+  });
+
+  test('should show login button after clicking menu button and direct to login page after clicking login button', () => {
+    const pushMock = jest.fn();
+    (useRouter as jest.Mock).mockReturnValue({
+      push: pushMock,
+    });
+
+    render(<MainNavigation size="full" />);
+    const menuButton = screen.getByAltText(/menu/i);
+
+    // Show login button
+    fireEvent.click(menuButton);
+    const loginButton = screen.getByRole('button', { name: /in/i });
+    expect(loginButton).toBeInTheDocument();
+
+    // Direct to login page
+    fireEvent.click(loginButton);
+    expect(pushMock).toHaveBeenCalledWith(`/${API_PATH.Login}`);
   });
 });
