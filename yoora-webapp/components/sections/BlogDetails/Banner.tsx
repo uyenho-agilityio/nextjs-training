820bd2663ac525b64091655959b368affb863eea
Uyen Ho <uyen.ho@asnet.com.vn>
2023-03-02 17:22:17 +0700

    Optimize responsive image

--- yoora-webapp/components/sections/BlogDetails/Banner.tsx
+++ yoora-webapp/components/sections/BlogDetails/Banner.tsx
@@ -12,21 +12,39 @@ import { BannerCard } from '@webapp/components';
 
 const BannerSection = (): JSX.Element => {
   return (
-    <Box bg="primary-bg">
-      <Container size="md">
-        <BannerCard {...DETAILS_CONTENT} />
-      </Container>
+    <>
+      {/* Mobile */}
+      <Box bg="primary-bg" display={{ lg: 'none' }}>
+        <Container size="md">
+          <BannerCard {...DETAILS_CONTENT} />
 
-      <Box display="flex" justifyContent="center">
-        <Image
-          {...DETAILS_CONTENT.image}
-          placeholder="blur"
-          blurDataURL={DETAILS_CONTENT.image.src}
-          sizes="(min-width: 320px) 100vw, 173px, (min-width: 768px) 100vw, 342px, (min-width: 1440px) 100vw, 511px"
-          loading="eager"
-        />
+          <Image
+            {...DETAILS_CONTENT.image}
+            placeholder="blur"
+            blurDataURL={DETAILS_CONTENT.image.src}
+            sizes="(min-width: 320px) 100vw, 173px, (min-width: 768px) 100vw, 342px, (min-width: 1440px) 100vw, 511px"
+            loading="eager"
+          />
+        </Container>
       </Box>
-    </Box>
+
+      {/* Desktop */}
+      <Box bg="primary-bg" display={{ base: 'none', lg: 'block' }}>
+        <Container size="md">
+          <BannerCard {...DETAILS_CONTENT} />
+        </Container>
+
+        <Box display="flex" justifyContent="center">
+          <Image
+            {...DETAILS_CONTENT.image}
+            placeholder="blur"
+            blurDataURL={DETAILS_CONTENT.image.src}
+            sizes="(min-width: 320px) 100vw, 173px, (min-width: 768px) 100vw, 342px, (min-width: 1440px) 100vw, 511px"
+            loading="eager"
+          />
+        </Box>
+      </Box>
+    </>
   );
 };
 
