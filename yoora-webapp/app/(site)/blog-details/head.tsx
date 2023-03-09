e11db1211d35a0b3caaa670bea5c34abb8b14965
Uyen Ho <uyen.ho@asnet.com.vn>
2023-03-09 08:41:33 +0700

    Fix: Avoid chaining critical requests

--- yoora-webapp/app/(site)/blog-details/head.tsx
+++ yoora-webapp/app/(site)/blog-details/head.tsx
@@ -4,6 +4,7 @@ const Head = () => {
       <title>Blog Details Page</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Blog Details Page" />
+      <link rel="preload" href="/_next/static/css/a6b765653498d7a5.css" as="style" />
     </>
   );
 };
