b408919cb286c4df6f2b9d4260441db0a4073b71
Uyen Ho <uyen.ho@asnet.com.vn>
2023-03-10 16:02:57 +0700

    Add @next/bundle-analyzer cross-env

--- yoora-webapp/next.config.js
+++ yoora-webapp/next.config.js
@@ -1,16 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const nextConfig = withBundleAnalyzer({
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
     appDir: true,
   },
-
   images: {
     domains: ['localhost', 'yoora-webapp.vercel.app'],
     deviceSizes: [320, 768, 1440],
   },
   optimizeFonts: true,
-};
+  env: {
+    NEXT_PUBLIC_ENV: 'PRODUCTION',
+  },
+});
 
 module.exports = nextConfig;
